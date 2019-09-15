6f38190039b3924a9a37fa334bee07201ffc5a5b
JOSEBRANDL <jbrandlondoo@yahoo.es>
2019-09-15 17:34:25 -0500

    Button Add Sell
    se creo el boton de iniciar venta

---
+++ src/components/table/TableResume.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import { withStyles, makeStyles } from '@material-ui/core/styles'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import Paper from '@material-ui/core/Paper'
+
+
+const StyledTableCell = withStyles(theme => ({
+  head: {
+    backgroundColor: '#574F7D',
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles(theme => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.background.default,
+    },
+  },
+}))(TableRow);
+
+const useStyles = makeStyles(theme => ({
+  tablescroll:{
+    overflow:'scroll',
+    height:500,
+    marginTop:20,
+  },
+  root: {
+    width: '100%',
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 500,
+  },
+}));
+
+/**
+ * 
+ * @param {*} props  este objeto debe tener ROWS:[{product,price,lot,code}]
+ */
+const TableResume = props => {
+  const classes = useStyles()
+  return (
+    <div className={classes.tablescroll}>
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <StyledTableCell align="right">Producto</StyledTableCell>
+            <StyledTableCell align="right">Total(COP)</StyledTableCell>
+            <StyledTableCell align="right">Cantidad</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {props.rows?props.rows.map(row => (
+                <StyledTableRow 
+                key={row.code}
+                hover
+                onClick={event => {console.log(row)}}>
+                <StyledTableCell align="right">{row.product}</StyledTableCell>
+                <StyledTableCell align="right">{row.price},00</StyledTableCell>
+                <StyledTableCell align="right">{row.lot}</StyledTableCell>
+                </StyledTableRow>
+          )):
+            null
+          }
+        </TableBody>
+      </Table>
+    </Paper>
+    </div>
+  );
+}
+
+export default TableResume;
