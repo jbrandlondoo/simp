b09cf7180130ca7f346b614a0ef8d9d180b2d56a
Santiago Sanchez <santisanchez.1214@gmail.com>
2019-09-19 00:03:53 -0500

    Fixed error on auth token save

--- src/store/reducers/authentication.js
+++ src/store/reducers/authentication.js
@@ -5,7 +5,7 @@ const initialState = {};
 export const authentication = (state = initialState, action) =>{
     switch(action.type){
         case actions.LOGIN:
-            return Object.assign({},action.payload)
+            return action.payload
         case actions.LOGOUT:
             return initialState
         default:
