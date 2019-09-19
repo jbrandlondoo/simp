466316e6b87269f465b8abed59d8e18905f5f812
Santiago Sanchez <santisanchez.1214@gmail.com>
2019-09-19 00:36:48 -0500

    Moved onSubmit method and deleted commented code
    Also added storeId of user on login

--- src/components/form/login/login.js
+++ src/components/form/login/login.js
@@ -16,28 +16,6 @@ import Axios from 'axios';
  * @returns {null}
  */
 
-const onSubmit = async values => {
-  Axios.post(config.URL_API+':'+config.PORT_API+'/api/v1/login',values).then((response)=>{
-    let data = response.data.success;
-    values.login({token:data.token,error:'',userId:data.id,name:data.name})
-  })
-  // let res = ''
-  // let token = ''
-  // let data = values;
-  // let headers = {
-  //   method: 'POST',
-  //   body: JSON.stringify(data)
-  // }
-  // try{
-  //   res = await fetch(config.URL_API+':'+config.PORT_API+'/api/v1/login',config.HEADERS)
-  //   // values.login({ token:res.data.success.token,error:'',userId:1,name:'juan', storeName:'kakashi'})
-  //   // localStorage.setItem('authentication', values.authentication)
-  //   console.log(res);
-  // }catch(err){
-  //   console.log(err)
-  // }
-}
-
 /**
  * @description 
  * @param {*} values 
@@ -104,7 +82,7 @@ const Login = props => {
   const onSubmit = async values => {
     Axios.post(config.URL_API+':'+config.PORT_API+'/api/v1/login',values).then((response)=>{
       let data = response.data.success;
-      props.login({token:data.token,error:'',userId:data.id,name:data.name})
+      props.login({token:data.token,error:'',userId:data.id,name:data.name,storeId:data.store.id})
       localStorage.setItem('authentication', JSON.stringify({token:data.token,error:'',userId:data.id,name:data.name}))
     })
   }
