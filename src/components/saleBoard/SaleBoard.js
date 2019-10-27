d98187cc1ad506b8f36ac1fce8a908d432642a38
Juan Pablo Cano Cuadros <53202964+jpcc1217@users.noreply.github.com>
2019-10-27 11:50:44 -0500

    i dont know the fuck im doing

--- src/components/saleBoard/SaleBoard.js
+++ src/components/saleBoard/SaleBoard.js
@@ -14,29 +14,29 @@ import {
 } from './../../store/action/index'
 import Axios from 'axios';
 
-const styles ={
-    main:{
-        background:'#FFFFFF',
-        height:'100%',
-        width:500,
-        margin:20,
-    },
-    headerSale:{
-        fontFamily:'Roboto',
-        fontSize:24,
-        width:'100%',
-        textAlign:'center',
-        height:30,
-        padding:5,
-    }
+const styles = {
+  main: {
+    background: '#FFFFFF',
+    height: '100%',
+    width: 500,
+    margin: 20,
+  },
+  headerSale: {
+    fontFamily: 'Roboto',
+    fontSize: 24,
+    width: '100%',
+    textAlign: 'center',
+    height: 30,
+    padding: 5,
+  }
 }
 
 const useStyles = makeStyles(theme => ({
   fab: {
     margin: theme.spacing(1),
-    background:'#574F7D',
-    color:'#FFFFFF',
-    alignSelf:'center'
+    background: '#574F7D',
+    color: '#FFFFFF',
+    alignSelf: 'center'
   },
   button: {
     margin: theme.spacing(1),
@@ -54,53 +54,53 @@ const SaleBoard = props => {
   const classes = useStyles();
   const [sell, setSell] = useState(false)
 
-  const sellProducts = ()=>{
-    Axios.post(config.URL_API+'/api/v1/sellProducts',{store_id:props.authentication.storeId,products:props.products},{headers:{'Authorization':'Bearer ' + props.authentication.token}}).then((response)=>{
+  const sellProducts = () => {
+    Axios.post(config.URL_API + '/api/v1/sellProducts', { store_id: props.authentication.storeId, products: props.products }, { headers: { 'Authorization': 'Bearer ' + props.authentication.token } }).then((response) => {
       alert("Venta Exitosa!");
-  });
+    });
   }
   return (
     <Box
-    boxShadow={0}
-    style={styles.main}
-  >
-      <Box style={styles.headerSale} >{sell?`Venta ${new Date().getTime()}`:'Iniciar Venta'}</Box>
-      <hr style={{background:'background: rgba(0, 0, 0, 0.12)'}}/>
+      boxShadow={0}
+      style={styles.main}
+    >
+      <Box style={styles.headerSale} >{sell ? `Venta ${new Date().getTime()}` : 'Iniciar Venta'}</Box>
+      <hr style={{ background: 'background: rgba(0, 0, 0, 0.12)' }} />
       <Box>
         {
-          sell?
-        <Box>
-          <TableResume/>
-          <Button variant="outlined"
-           onClick={props.delSelectedProduct}
-           disabled={props.products.filter(item=>item.selected).length?false:true} className={classes.button}>
-            Eliminar
+          sell ?
+            <Box>
+              <TableResume />
+              <Button variant="outlined"
+                onClick={props.delSelectedProduct}
+                disabled={props.products.filter(item => item.selected).length ? false : true} className={classes.button}>
+                Eliminar
           </Button>
-          <Button onClick={sellProducts} variant="outlined" color="primary" disabled={props.products.length?false:true} className={classes.button}>
-            Generar Factura
+              <Button onClick={sellProducts} variant="outlined" color="primary" disabled={props.products.length ? false : true} className={classes.button}>
+                Generar Factura
           </Button>
-          <Button variant="outlined"
-          onClick={props.cancelSell}
-          disabled={props.products.length?false:true}
-          color="secondary" className={classes.button}>
-           Cancelar
+              <Button variant="outlined"
+                onClick={props.cancelSell}
+                disabled={props.products.length ? false : true}
+                color="secondary" className={classes.button}>
+                Cancelar
           </Button>
-        </Box>
-          :
-        <Fab aria-label="add" onClick={()=>setSell(true)} className={classes.fab}>
-          <AddIcon />
-        </Fab>}
+            </Box>
+            :
+            <Fab aria-label="add" onClick={() => setSell(true)} className={classes.fab}>
+              <AddIcon />
+            </Fab>}
       </Box>
-  </Box>
+    </Box>
   );
 }
 
 const mapStateToProps = state => ({
-  products:state.products.products,
-  authentication:state.authentication
+  products: state.products.products,
+  authentication: state.authentication
 })
 const mapDispatchToProps = dispatch => ({
-  delSelectedProduct:()=>dispatch(delSelectedProduct()),
-  cancelSell:()=>dispatch(cancelSell()),
+  delSelectedProduct: () => dispatch(delSelectedProduct()),
+  cancelSell: () => dispatch(cancelSell()),
 })
-export default connect(mapStateToProps,mapDispatchToProps)(SaleBoard)
+export default connect(mapStateToProps, mapDispatchToProps)(SaleBoard)
